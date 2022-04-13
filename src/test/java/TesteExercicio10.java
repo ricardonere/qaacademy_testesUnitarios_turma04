05b099eb933f7985a256038f806130126528981e
Ricardo <ricardonere@gmail.com>
2022-04-13 17:57:53 -0300

    Teste unitário Exercicio10 easy

--- src/test/java/TesteExercicio10.java
+++ src/test/java/TesteExercicio10.java
@@ -1,2 +1,16 @@
+import easy.Exercicio10;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.text.DecimalFormat;
+
 public class TesteExercicio10 {
+    @Test
+    public void testecalcularInvestimento(){
+
+        Exercicio10 exercicio10 = new Exercicio10();
+        double valorJuros = exercicio10.calcularInvestimento(10000, 0.05);
+        DecimalFormat formato = new DecimalFormat("#,###.00");
+        Assert.assertEquals("5.000,00", formato.format(valorJuros));
+    }
 }
