4d80f9388fdd30ea45bbf4de47bb852568c593bf
Ricardo Nere <ricardonere@gmail.com>
2022-04-17 15:04:31 -0300

    Teste Exercicio04Medium

--- src/test/java/TesteExercicio04.java
+++ src/test/java/TesteExercicio04.java
@@ -2,13 +2,13 @@ import easy.Exercicio04;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class TesteExercicio04 {
+public class TesteExercicio04 {// Classe
 
-    @Test
-    public void testecalcularDobro(){
+    @Test// Indica que o método é um teste
+    public void testecalcularDobro(){// Chamada do método
 
-        Exercicio04 exercicio04 = new Exercicio04();
-        Assert.assertEquals(20, exercicio04.calcularDobro(10));
+        Exercicio04 exercicio04 = new Exercicio04(); // Cria a Instância da classe
+        Assert.assertEquals(20, exercicio04.calcularDobro(10)); // Compara o resultado esperado com o atual
 
     }
 }
