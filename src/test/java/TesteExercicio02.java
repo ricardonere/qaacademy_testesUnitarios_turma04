399b306bca5d88403a3150f48a38e23dff041210
Ricardo Nere <ricardonere@gmail.com>
2022-04-17 15:01:58 -0300

    Teste unitário Exercicio 02Medium.java

--- src/test/java/TesteExercicio02.java
+++ src/test/java/TesteExercicio02.java
@@ -2,10 +2,10 @@ import easy.Exercicio02;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class TesteExercicio02 {
-    @Test
-    public void testePalavraDigitada() {
-        Exercicio02 exercicio02 = new Exercicio02();
-        Assert.assertEquals("Voce digitou: Teste", exercicio02.retornaPalavraDigitada("Teste"));
+public class TesteExercicio02 {//Classe
+    @Test// Indica que o método é um teste
+    public void testePalavraDigitada() { // Chamada do Método
+        Exercicio02 exercicio02 = new Exercicio02(); // Cria a Instância
+        Assert.assertEquals("Voce digitou: Teste", exercicio02.retornaPalavraDigitada("Teste"));// Compara o resutado esperado com o atual
     }
 }
