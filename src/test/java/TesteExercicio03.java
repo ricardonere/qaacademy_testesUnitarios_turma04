f1962ea877816befde04023012ec8a8188138b3c
Ricardo Nere <ricardonere@gmail.com>
2022-04-17 15:03:25 -0300

    Teste unitário Exercicio 03Medium

--- src/test/java/TesteExercicio03.java
+++ src/test/java/TesteExercicio03.java
@@ -2,15 +2,16 @@ import easy.Exercicio03;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class TesteExercicio03 {
+public class TesteExercicio03 {// Classe
 
-    @Test
-    public void testeTxibeValoresTrocados(){
+    @Test //Indica que o método é um teste
+    public void testeTxibeValoresTrocados(){ // Chamada do Método
 
         String mensagem =  "O valor do primeiro numero é :20\n"
-                +"O valor do segundo numero é :10";
-        Exercicio03 exercicio03 = new Exercicio03();
-        Assert.assertEquals(mensagem, exercicio03.exibeValoresTrocados(10, 20));
+                +"O valor do segundo numero é :10"; //Retorna Mensagem
+        Exercicio03 exercicio03 = new Exercicio03(); //Cria a Instância
+        Assert.assertEquals(mensagem, exercicio03.exibeValoresTrocados(10, 20));// Compara o resultado esperado com o atual
+
 
     }
 }
--- src/test/java/TesteExercicio03Medium.java
+++ src/test/java/TesteExercicio03Medium.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class TesteExercicio03Medium {
+import medium.Exercicio03;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TesteExercicio03Medium {
+    static Exercicio03 exercicio03;
+
+    @BeforeClass
+    public static void before(){
+        exercicio03 = new Exercicio03();
+    }
+    @Test
+    public void testeSoma(){
+        exercicio03.somaAteMil();
+    }
 }
